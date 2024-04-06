33207288aca4fbd89b60446007bb521d1787873e
Ola <olamiwalepaul@gmail.com>
2024-04-06 15:00:23 +0100

    project link added

--- app/project1/page.js
+++ app/project1/page.js
@@ -9,7 +9,7 @@ export default function page() {
       <PreviewProps 
       title='Netflix Clone'
       img1={'/pro--3.png'}
-      url1={'https://github.com/Olamiwale/Netflix-clone-with-firebasee'}
+      url1={'https://github.com/Olamiwale/Netflix-clone-with-firebasee.git'}
       url2={'https://google.com'}
       des='Streamify is a meticulously crafted Netflix app clone designed to redefine how you consume entertainment. By leveraging cutting-edge technology and intuitive design, Streamify offers an immersive streaming platform with a plethora of features tailored to enhance your viewing pleasure.'
       />
--- app/project2/page.js
+++ app/project2/page.js
@@ -10,7 +10,7 @@ export default function page() {
       <PreviewProps 
       title='Vanlife'
       img1={'/pro--1.png'}
-      url1={'https://github.com/Olamiwale/Netflix-clone-with-firebasee'}
+      url1={'https://github.com/Olamiwale/vanlife.git'}
       url2={'https://google.com'}
       des='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       />
